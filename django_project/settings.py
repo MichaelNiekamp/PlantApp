3ca574a70631bcdb00d2a22c00ef71388a160be2
jaycehold <76455497+jaycehold@users.noreply.github.com>
2022-12-10 15:52:19 -0700

    Restarted repo - Jayce (#5)
    * Restarted repo - Jayce
    
    * Removed dupplicate static files

--- django_project/settings.py
+++ django_project/settings.py
@@ -64,8 +64,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware"
-
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "django_project.urls"
@@ -135,7 +134,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Static files for deployment
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
